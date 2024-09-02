cbfa3c50102ad85406fdfbba1da5e9539dde0c3b
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-09-02 20:16:54 +0100

    Remove extra code - keep v simple

--- testing_app/lib/main.dart
+++ testing_app/lib/main.dart
@@ -1,49 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
-import 'models/favorites.dart';
-import 'screens/favorites.dart';
-import 'screens/home.dart';
 
 void main() {
-  runApp(const TestingApp());
+  runApp(const MainApp());
 }
 
-final _router = GoRouter(
-  routes: [
-    GoRoute(
-      path: HomePage.routeName,
-      builder: (context, state) {
-        return const HomePage();
-      },
-      routes: [
-        GoRoute(
-          path: FavoritesPage.routeName,
-          builder: (context, state) {
-            return const FavoritesPage();
-          },
-        ),
-      ],
-    ),
-  ],
-);
-
-class TestingApp extends StatelessWidget {
-  const TestingApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Favorites>(
-      create: (context) => Favorites(),
-      child: MaterialApp.router(
-        title: 'Testing Sample',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-          ),
-          useMaterial3: true,
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
         ),
-        routerConfig: _router,
       ),
     );
   }
